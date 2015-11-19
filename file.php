a37b00369e3eb1a552c8f03ffd733de9cfcb3314
Uncater <leonid.neugodnikov@gmail.com>
2015-11-19 13:37:58 +0200

    Update file.php

--- file.php
+++ file.php
@@ -4,10 +4,10 @@
 		<title>*Uploading file*</title>
 	</head>
 	<body>
-<form action=handler.php method=post enctype=multipart/form-data>
+<form action="handler.php" method="post" enctype=multipart/form-data>
 <label for="folder">Type folder name here:</label>
 <input id="folder" type="text" name="folder_name"><br>
-<input type="file" name="file" value="Let's go!"><br>
+<input type="file" name="file"><br>
 <input type="submit" value="Upload"></form>
   </body>
  <html>
